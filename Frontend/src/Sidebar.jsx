1d9c4a33420b60d7f88d31038769d36477f2d924
Daniyal Ahmad <123984261+Daniyalahmad07@users.noreply.github.com>
2026-01-09 09:16:37 +0530

    Update Sidebar.jsx

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -9,7 +9,10 @@ function Sidebar(){
 
     const getAllThreads = async () =>{
         try{
-            const response = await fetch("http://localhost:8080/api/thread");
+            const response = await fetch(
+  `${import.meta.env.VITE_API_URL}/api/thread`
+);
+
             const res = await response.json();
             const filteredData = res.map(thread => ({threadId: thread.threadId,title: thread.title}));
             // console.log(filteredData);
@@ -35,7 +38,10 @@ function Sidebar(){
     const changeThread=async (newThreadId)=>{
         setCurrThreadId(newThreadId);
         try{
-            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+            const response = await fetch(
+  `${import.meta.env.VITE_API_URL}/api/thread/${newThreadId}`
+);
+
             const res = await response.json();
             console.log(res);
             setPrevChats(res);
@@ -50,7 +56,11 @@ function Sidebar(){
 
     const deleteThread = async (threadId) =>{
         try{
-            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`,{method:"DELETE"});
+            await fetch(
+  `${import.meta.env.VITE_API_URL}/api/thread/${threadId}`,
+  { method: "DELETE" }
+);
+
             const res = await response.json();
             console.log(res);
 
